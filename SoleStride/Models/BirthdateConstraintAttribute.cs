4d1c0b1b0ad4de34e9009c506c8be8ff416aa440
IRISnoir <hoangphantom2468@gmail.com>
2026-07-20 23:31:19 +0700

    feat: Tạo model User, và thêm các trường dữ liệu cho người dùng

---
+++ SoleStride/Models/BirthdateConstraintAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoleStride.Models
+{
+    internal class BirthdateConstraintAttribute : ValidationAttribute
+    {
+        //Minimum age 14
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTime birthdate)
+            {
+                var age = DateTime.Now.Year - birthdate.Year;
+                if (age < 14)
+                {
+                    return new ValidationResult("You must be at least 14 years old.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ SoleStride/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoleStride.Models
+{
+    public class User
+    {
+        [Key]
+        [MaxLength(255)]
+        public string Username { get; set; }
+
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        public enum UserRole
+        {
+            Admin,
+            Staff,
+            User
+        }
+        [Required]
+        public UserRole Role { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+
+        [BirthdateConstraint]
+        [Required]
+        public DateTime Birthdate { get; set; } = DateTime.Now - TimeSpan.FromDays(14 * 365);
+
+        public enum Gender { Male, Female }
+        [Required]
+        public Gender UserGender { get; set; }
+    }
+}
